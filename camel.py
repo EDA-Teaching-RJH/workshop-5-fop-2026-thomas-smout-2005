29b915c1c6d17fc98737dbfd220eaac81b1044f9
Thomas Smout <ts734@kent.ac.uk>
2026-02-25 20:15:50 +0000

    finished first 2 parts

--- camel.py
+++ camel.py
@@ -1,1 +1,19 @@
-# Enter your code here
+import re
+
+"""
+name = input("name please")
+
+s1 = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', name)
+s2 = re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s1)
+
+print(s2.lower())
+"""
+
+word = input("Enter word : ")
+
+for char in word:
+    if char.isupper():
+        print("_", char.lower(), sep="", end="")
+    else:
+        print(char, end="")
+print("")
--- coffee.py
+++ coffee.py
@@ -1,1 +1,31 @@
-#Your code goes here
+import math
+
+print("This machine only takes 50p, 20p, 10p and 5p")
+
+
+while True:
+    current_value = int(input("Insert coin "))
+    if current_value == 5 or current_value == 10 or current_value == 20 or current_value == 50:
+        break
+    else:
+        print("Please enter a coin this machine accepts")
+
+
+remaining = 75 - current_value
+
+
+while current_value < 75:
+    print(f"You still need to insert {remaining}p")
+    
+    while True:
+        coins = int(input("Insert coin "))
+        if coins == 5 or coins == 10 or coins == 20 or coins == 50:
+            break
+        else:
+            print("Please enter a coin this machine accepts")
+    
+    current_value = current_value + coins
+    remaining = 75 - current_value
+
+
+print(f"Heres your coffee and your change of {-remaining}p")
