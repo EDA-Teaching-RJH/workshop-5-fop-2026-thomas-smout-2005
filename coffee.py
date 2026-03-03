182fcdabd160e7a1cb39a01946503baed1bfd589
Thomas Smout <ts734@kent.ac.uk>
2026-03-03 11:23:22 +0000

    part 5

--- coffee.py
+++ coffee.py
@@ -4,6 +4,10 @@ import math
 # When printing the results, or an updates in price, it uses a print(f) command to read the integer and output it
 # Program should only accept an integer input at any point but should be able to check if input is or isn't an integer
 
+
+coin_return = []
+
+
 def main():
     print("This machine only takes 50p, 20p, 10p and 5p")
     current = 0
@@ -33,8 +37,29 @@ def update_total(current, coin):
     return current
 
 
+def calculate_change(amount):
+    remaining = amount - 75
+    if remaining == 0:
+        coin_return.append("0p")
+    while remaining != 0:
+        if remaining >= 50:
+            remaining = remaining - 50
+            coin_return.append("50p")
+        elif remaining >= 20:
+            remaining = remaining - 20
+            coin_return.append("20p")
+        elif remaining >= 10:
+            remaining = remaining - 10
+            coin_return.append("10p")
+        elif remaining >= 5:
+            remaining = remaining - 5
+            coin_return.append("5p")
+
+
 def dispense_product(current):
-    remaining = 75 - current
-    print(f"Heres your coffee and your change of {-remaining}p")
+    amount = current
+    calculate_change(amount)
+    print("Heres your coffee and your change of", ", ".join(coin_return))
+
 
 main()
